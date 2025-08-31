fcc0e27f0b898c357caa407f2c002f75e508051a
gani113 <saidgani113@gmail.com>
2025-08-31 22:21:24 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,34 @@
+resource "google_compute_network" "project-vpc" {
+    project = var.project
+    name = "project-vpc"
+    auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "project-vpc-subnet" {
+    name = "subnet-1"
+    network = google_compute_network.project-vpc.id
+    region = var.region
+    ip_cidr_range = "10.2.0.0/16"
+}
+
+resource "google_compute_firewall" "project-fw" {
+    name = "project-fw"
+    network = google_compute_network.project-vpc.id
+    source_ranges = ["0.0.0.0/0"]
+
+    allow {
+        protocol = "tcp"
+      	ports = ["22"]
+    }
+
+    allow {
+        protocol = "tcp"
+      	ports = ["80"]
+    }
+
+    allow {
+        protocol = "tcp"
+      	ports = ["443"]
+    }
+  
+}
