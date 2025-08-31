21b628b501630d72d4ac2208e91158d3d471e2ee
gani113 <saidgani113@gmail.com>
2025-08-31 22:21:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+    default = "binge-projects"
+    description = "This is Project_ID"
+    type = string  
+}
+
+variable "region" {
+    default = "us-central1"
+    description = "This is Region"
+    type = string
+}
