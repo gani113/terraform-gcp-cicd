d337eeaee10ec98797e17bc958edbad751f7ee76
gdurvasula <gdurvasula@gmail.com>
2025-08-31 22:54:23 +0530

    add changes

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "google_compute_network" "project-vpc" {
+resource "google_compute_network" "project_vpc" {
     project = var.project
     name = "project-vpc"
     auto_create_subnetworks = false
@@ -6,14 +6,14 @@ resource "google_compute_network" "project-vpc" {
 
 resource "google_compute_subnetwork" "project-vpc-subnet" {
     name = "subnet-1"
-    network = google_compute_network.project-vpc.id
+    network = google_compute_network.project_vpc.id
     region = var.region
     ip_cidr_range = "10.2.0.0/16"
 }
 
 resource "google_compute_firewall" "project-fw" {
     name = "project-fw"
-    network = google_compute_network.project-vpc.id
+    network = google_compute_network.project_vpc.id
     source_ranges = ["0.0.0.0/0"]
 
     allow {
